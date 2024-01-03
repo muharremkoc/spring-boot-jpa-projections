2ae43a8be728fb493d6d8e02504a8fc042e836fb
muharremkoc <kmuho5885@gmail.com>
2024-01-03 15:21:22 +0300

    Jpa-Projections-Beautified

--- src/main/java/com/spring/jpa/projections/model/UserDto.java
+++ src/main/java/com/spring/jpa/projections/model/UserDto.java
@@ -1,9 +1,13 @@
 package com.spring.jpa.projections.model;
 
+import org.springframework.beans.factory.annotation.Value;
+
 public interface UserDto {
 
      int getId();
      String getFirstName();
      String getLastName();
+     @Value("#{target.firstName + ' ' + target.lastName}")// bu target alanı repository'de çağrılan entity nesnesidir.
+     String getFullName();
 
 }
